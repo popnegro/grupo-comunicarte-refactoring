2de70f5126d33a54af7593595106bafebb2da945
popnegro <grasso.luis@gmail.com>
2026-08-24 15:19:33 -0300

    chore: remove obsolete Vercel serverless API entrypoint

--- api/index.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from '../dist/server.cjs';
-
-const appPromise = createApp();
-
-export default async function handler(req: any, res: any) {
-  const app = await appPromise;
-  return app(req, res);
-}
