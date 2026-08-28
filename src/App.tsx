3abd6fbdf97f1aaff5e264da0287837bff36fc20
popnegro <grasso.luis@gmail.com>
2026-08-28 17:56:19 -0300

    refactor(ux): route support list add and edit separately

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,8 @@ import Contacto from './pages/Contacto';
 // Auth & Dashboard
 import Login from './pages/auth/Login';
 import Dashboard from './pages/dashboard/Dashboard';
-import DashboardSoportes from './pages/dashboard/DashboardSoportes';
+import DashboardSupportList from './pages/dashboard/DashboardSupportList';
+import DashboardSupportEditor from './pages/dashboard/DashboardSupportEditor';
 import DashboardMediaKitWorkflow from './pages/dashboard/DashboardMediaKitWorkflow';
 
 function PublicRoutes() {
@@ -48,7 +49,10 @@ export default function App() {
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/dashboard/soportes" element={<DashboardSoportes />} />
+          <Route path="/dashboard/soportes" element={<DashboardSupportList />} />
+          <Route path="/dashboard/soportes/new" element={<DashboardSupportEditor mode="create" />} />
+          <Route path="/dashboard/soportes/:canonicalId/edit" element={<DashboardSupportEditor mode="edit" />} />
+          <Route path="/dashboard/soportes/legacy" element={<div className="hidden"><DashboardSupportList /></div>} />
           <Route path="/dashboard/solicitudes" element={<DashboardMediaKitWorkflow />} />
           <Route path="/dashboard/mediakits" element={<DashboardMediaKitWorkflow />} />
           <Route path="*" element={<PublicRoutes />} />
