146e188ab594f5091f5ab2f013a26e682d10e7ec
popnegro <grasso.luis@gmail.com>
2026-08-31 02:08:49 -0300

    fix: render connected media uploader inside dashboard shell

--- src/pages/dashboard/DashboardSupportProductEditorConnected.tsx
+++ src/pages/dashboard/DashboardSupportProductEditorConnected.tsx
@@ -1,17 +1,19 @@
 import DashboardSupportProductEditor from './DashboardSupportProductEditor';
 import DashboardSupportMediaUpload from './DashboardSupportMediaUpload';
 import { useParams } from 'react-router-dom';
+import { DashboardShell } from '../../components/dashboard/DashboardShell';
 
 export default function DashboardSupportProductEditorConnected({ mode }: { mode?: 'create' | 'edit' }) {
   const { canonicalId } = useParams();
+
   return (
-    <>
+    <DashboardShell>
       <DashboardSupportProductEditor mode={mode} />
       {canonicalId && (
         <div className="mx-auto max-w-7xl px-4 pb-14 sm:px-6 lg:px-8">
           <DashboardSupportMediaUpload canonicalId={canonicalId} />
         </div>
       )}
-    </>
+    </DashboardShell>
   );
 }
